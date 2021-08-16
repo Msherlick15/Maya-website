00c5f55ed00fc798998fb922dc9070f0ad3015da
Maya <mayaluna@sherlick.com>
2021-08-16 17:43:12 -0400

    Test hit and miss commands

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,5 +1,15 @@
 var view = {
   displayMessage: function(msg) {
-      var messageArea = document.getElementById("messageArea");
+      var messageArea = document.getElementById("Tap tap, is this thing on?");
       messageArea.innerHTML = msg;
 }
+displayHit: function(location) {
+  var cell = document.getElementById(location); 
+  cell.setAttribute("class", "hit");
+}
+displayMiss: function(location) {
+  var cell = document.getElementById(location);
+  cell.setAttribute("class", "miss");
+}
+
+view.displayMiss("00"); view.displayHit("34"); view.displayMiss("55"); view.displayHit("12"); view.displayMiss("25"); view.displayHit("26");
